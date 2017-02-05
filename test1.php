333383df44538a9356b5ef8baced2c2c04ef0417
magz1984 <magz1984@gmail.com>
2017-02-05 18:42:24 +0530

    2nd commit

--- test1.php
+++ test1.php
@@ -4,7 +4,7 @@
 
 
   <form action="test.php" method="post">
-    Email : <input type="text" name="email">
+    Email : <input type="text" name="emailm">
 
     Password : <input type="password" name="password">
 
